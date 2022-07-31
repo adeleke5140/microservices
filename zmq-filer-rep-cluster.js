2b40113bf37187d1ec461092d6c4ad613477389f
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-07-31 05:47:53 +0100

    deal with clusters in zeromq

--- zmq-filer-rep-cluster.js
+++ zmq-filer-rep-cluster.js
@@ -1,2 +1,43 @@
 //why do we need to spin off worker processes in the first place
 //is it in order for us to be able to spin off multiple processes since NodeJS is single threaded
+
+"use strict"
+const cluster = require("cluster")
+const fs = require("fs")
+const zmq = require("zeromq")
+
+const numWorkers = require("os").cpus().length
+
+if (cluster.isMaster) {
+  const router = zmq.socket("router").bind("tcp://127.0.0.1:60401")
+  const dealer = zmq.socket("dealer").bind("ipc://filer-dealer.ipc")
+
+  router.on("message", (...frames) => dealer.send(frames))
+  dealer.on("message", (...frames) => router.send(frames))
+
+  cluster.on("online", (worker) =>
+    console.log(`Worker ${worker.process.id} is online.`)
+  )
+
+  for (let i = 0; i < numWorkers; i++) {
+    cluster.fork()
+  }
+} else {
+  const responder = zmq.socket("rep").connect("ipc://filer-dealer.ipc")
+
+  responder.on("message", (data) => {
+    const request = JSON.parse(data)
+    console.log(`${process.id} received request for: ${request.path}`)
+
+    fs.readFile(request.path, (err, content) => {
+      console.log(`${process.id} sending response`)
+      responder.send(
+        JSON.stringify({
+          content: content.toString(),
+          timestamp: Date.now(),
+          pid: process.pid
+        })
+      )
+    })
+  })
+}
